9ffd0f87d6a57932a09109b6807c8630abdb3a53
Rohit Ahuja <hackerrohit101@gmail.com>
2018-06-12 01:19:47 +0530

    Confusion Matrix

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -27,3 +27,7 @@ classifier.fit(X_train,y_train)
 
 #Predicting the test set result
 y_pred = classifier.predict(X_test)
+
+#Making Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
